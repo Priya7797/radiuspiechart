bdd3af18ee426d64a3499744c564c2910f7e7b68
Priya7797 <81359264+Priya7797@users.noreply.github.com>
2021-04-19 15:30:41 +0530

    Update demopiechart.js

--- demopiechart.js
+++ demopiechart.js
@@ -1,17 +1,4 @@
-/**
- * ---------------------------------------
- * This demo was created using amCharts 4.
- * 
- * For more information visit:
- * https://www.amcharts.com/
- * 
- * Documentation is available at:
- * https://www.amcharts.com/docs/v4/
- * ---------------------------------------
- */
- 
-(function() { 
-	
+(function()  {
 	let _series1Color;
 	let _chartTitle;
 	let _chartTitleFontSize;
@@ -19,13 +6,13 @@
 	const amchartschartsjs = "https://cdn.amcharts.com/lib/4/charts.js";
 	const amchartsanimatedjs = "https://cdn.amcharts.com/lib/4/themes/animated.js"
 	
-	let template = document.createElement("template");
-	template.innerHTML = `
-		<div id="chartTitle" style=""></div><br/>
-		<div id="chartdiv"></div>
-	`;
-
-	function loadScript(src) {
+	
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+<div id="chartdiv"></div>
+    `;
+	
+function loadScript(src) {
 		return new Promise(function(resolve, reject) {
 			let script = document.createElement('script');
 			script.src = src;
@@ -37,23 +24,25 @@
 
 			document.head.appendChild(script)
 		});
-	}
-	
-	class RadialBarChart extends HTMLElement {
+	}	
+
+    customElements.define('com-sap-sample-piechart1', class WidgetTemplate extends HTMLElement {
+
+
 		constructor() {
 			super(); 
+		
 			let shadowRoot = this.attachShadow({mode: "open"});
-			shadowRoot.appendChild(template.content.cloneNode(true));
-			this.addEventListener("click", event => {
-				var event = new Event("onClick");
-				this.dispatchEvent(event);
-			});
+			shadowRoot.appendChild(tmpl.content.cloneNode(true));					
 			this._props = {};
 			this._firstConnection = 0;
 		}
-		
-		connectedCallback() {
-			if (this._firstConnection === 0) {
+
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback(){
+			
+            if (this._firstConnection === 0) {
 				async function LoadLibs(that) {
 					try {
 						await loadScript(amchartscorejs);
@@ -68,39 +57,57 @@
 				}
 				LoadLibs(this);
 			}
-		}
+        }
 
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback(){
+        
+        }
 		onCustomWidgetBeforeUpdate(changedProperties) {
-			this._props = { ...this._props, ...changedProperties };
-		}
-		
-		onCustomWidgetAfterUpdate(changedProperties) {
-			if ("color" in changedProperties) {
-				this._series1Color = changedProperties["color"];
+				this._props = { ...this._props, ...changedProperties };
 			}
-			if ("title" in changedProperties) {
-				this._chartTitle = changedProperties["title"];
+
+			onCustomWidgetAfterUpdate(changedProperties) {
+				if ("color" in changedProperties) {
+					this._series1Color = changedProperties["color"];
+				}
+				if ("title" in changedProperties) {
+					this._chartTitle = changedProperties["title"];
+				}
+				if ("titlefontsize" in changedProperties) {
+					this._chartTitleFontSize = changedProperties["titlefontsize"];
+				}
+				if (this._firstConnection === 1) {
+					this.loadthis();
+				}
 			}
-			if ("titlefontsize" in changedProperties) {
-				this._chartTitleFontSize = changedProperties["titlefontsize"];
-			}
-			if (this._firstConnection === 1) {
-				this.loadthis();
-			}
-		}
-		
+
 		onCustomWidgetResize(width, height){
 			if (this._firstConnection === 1) {
 				this.loadthis();
 			}
         }
 		
-		loadthis() {
+        
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy(){
+        }
+
+        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+        /*
+        onCustomWidgetResize(width, height){
+        
+        }
+        */
+
+        loadthis(){
 			
 			let myChart = this.shadowRoot.getElementById('chartdiv');
 			myChart.style.height = this.shadowRoot.host.clientHeight - 20 + "px";
 			myChart.style.width = this.shadowRoot.host.clientWidth - 20 + "px";
-			
+		
 			if(this._chartTitle && this._chartTitle.trim() !== "") {
 				var chartTitle = this.shadowRoot.getElementById('chartTitle');
 				chartTitle.innerText = this._chartTitle.trim();
@@ -110,44 +117,47 @@
 				myChart.style.height = myChart.clientHeight - chartTitle.clientHeight - 10 + "px";
 				myChart.style.top = chartTitle.clientHeight - 10 + "px"; 
 			}
-			
-			// Themes begin
-			am4core.useTheme(am4themes_animated);
-			// Themes end
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
 
-			
-			var chart = am4core.create(myChart, am4charts.RadarChart);
-			chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+// Create chart
+var chart = am4core.create(myChart, am4charts.PieChart);
+chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
 
-			if(this.datasourceString.trim() === "{}") {
-				chart.data = [
+if(this.datasourceString.trim() === "{}") { 
 
-				  {
-				    country: "Lithuania",
-				    value: 260
-				  },
-				  {
-				    country: "Czechia",
-				    value: 230
-				  },
-				  {
-				    country: "Ireland",
-				    value: 200
-				  },
-				  {
-				    country: "Germany",
-				    value: 165
-				  },
-				  {
-				    country: "Australia",
-				    value: 139
-				  },
-				  {
-				    country: "Austria",
-				    value: 128
-				  }
-				];
-			} else {
+chart.data = [
+  {
+    country: "Lithuania",
+    value: 260
+  },
+  {
+    country: "Czechia",
+    value: 230
+  },
+  {
+    country: "Ireland",
+    value: 200
+  },
+  {
+    country: "Germany",
+    value: 165
+  },
+  {
+    country: "Australia",
+    value: 139
+  },
+  {
+    country: "Austria",
+    value: 128
+  }
+];
+
+
+}
+
+else {
 				var newDataSourceObj = JSON.parse(this.datasourceString);
 				var newChartData = [];
 				for(var i = 0; i < newDataSourceObj.length; i++) {
@@ -157,10 +167,10 @@
 					if(!newChartData.find(x => x.category_id === dimMemberID)) {
 						var newDataObject = {};
 						newDataObject.category_id = dimMemberID;
-						newDataObject.category = dimMemberDesc;
+						newDataObject.country = dimMemberDesc;
 						newDataObject.measuredescriptions = [];
 						newDataObject.measuredescriptions.push(msrObj.measure_description);
-						newDataObject.value1 = msrObj.formattedValue;
+						newDataObject.value = msrObj.formattedValue;
 						newChartData.push(newDataObject);
 					} else {
 						var existingObj = newChartData.find(x => x.category_id === dimMemberID);
@@ -168,27 +178,28 @@
 						var newProp = "value"+existingObj.measuredescriptions.length;
 						existingObj[newProp] = msrObj.formattedValue;
 					}
+					
 				}
 				chart.data = newChartData;
-			}
-			
-			var series = chart.series.push(new am4charts.PieSeries());
-			series.dataFields.value = "value";
-			series.dataFields.radiusValue = "value";
-			series.dataFields.category = "country";
-			series.slices.template.cornerRadius = 6;
-			series.colors.step = 3;
+				console.log("That's my new data for chart");
+				console.log(newChartData);
 
-			series.hiddenState.properties.endAngle = -90;
+}
 
-			chart.legend = new am4charts.Legend();
+var series = chart.series.push(new am4charts.PieSeries());
+series.dataFields.value = "value";
+series.dataFields.radiusValue = "value";
+series.dataFields.category = "country";
+series.slices.template.cornerRadius = 6;
+series.colors.step = 3;
 
-			
-			
+series.hiddenState.properties.endAngle = -90;
 
-		}
-	
-	}
-
-	customElements.define("com-bva4kor-sac-radialbarchart", RadialBarChart);
+chart.legend = new am4charts.Legend();
+// end am4core.ready()
+        }
+    
+    
+    });
+        
 })();
