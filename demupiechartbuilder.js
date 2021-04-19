59c5f3b202b936b170cfd073647487e21219bb5b
Priya7797 <81359264+Priya7797@users.noreply.github.com>
2021-04-19 20:13:52 +0530

    Update demupiechartbuilder.js

--- demupiechartbuilder.js
+++ demupiechartbuilder.js
@@ -10,7 +10,67 @@
 				<tr>
 					<td>Chart Title Font Size</td>
 					<td><input id="chart_title_fontsize" type="number" size="2" maxlength="2"></td>
-				</tr>
+				</tr
+
+
+<div class="onoffswitch">
+    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" tabindex="0" checked>
+    <label class="onoffswitch-label" for="myonoffswitch">
+        <span class="onoffswitch-inner"></span>
+        <span class="onoffswitch-switch"></span>
+    </label>
+</div>
+<style>
+.onoffswitch {
+    position: relative; width: 90px;
+    -webkit-user-select:none; -moz-user-select:none; -ms-user-select: none;
+}
+.onoffswitch-checkbox {
+    position: absolute;
+    opacity: 0;
+    pointer-events: none;
+}
+.onoffswitch-label {
+    display: block; overflow: hidden; cursor: pointer;
+    border: 2px solid #999999; border-radius: 20px;
+}
+.onoffswitch-inner {
+    display: block; width: 200%; margin-left: -100%;
+    transition: margin 0.3s ease-in 0s;
+}
+.onoffswitch-inner:before, .onoffswitch-inner:after {
+    display: block; float: left; width: 50%; height: 30px; padding: 0; line-height: 30px;
+    font-size: 14px; color: white; font-family: Trebuchet, Arial, sans-serif; font-weight: bold;
+    box-sizing: border-box;
+}
+.onoffswitch-inner:before {
+    content: "ON";
+    padding-left: 10px;
+    background-color: #34A7C1; color: #FFFFFF;
+}
+.onoffswitch-inner:after {
+    content: "OFF";
+    padding-right: 10px;
+    background-color: #EEEEEE; color: #999999;
+    text-align: right;
+}
+.onoffswitch-switch {
+    display: block; width: 18px; margin: 6px;
+    background: #FFFFFF;
+    position: absolute; top: 0; bottom: 0;
+    right: 56px;
+    border: 2px solid #999999; border-radius: 20px;
+    transition: all 0.3s ease-in 0s; 
+}
+.onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-inner {
+    margin-left: 0;
+}
+.onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-switch {
+    right: 0px; 
+}
+</style>
+
+
 			</table>
 			<input type="submit" style="display:none;">
 		</form>
@@ -36,7 +96,8 @@
 					detail: {
 						properties: {
 							title: this.title,
-							titlefontsize: this.titlefontsize
+							titlefontsize: this.titlefontsize, 
+							legends: this.legends
 						}
 					}
 			}));
@@ -57,6 +118,14 @@
 		get titlefontsize() {
 			return this._shadowRoot.getElementById("chart_title_fontsize").value;
 		}
+		set legends(newLegends) {
+			this._shadowRoot.getElementById("myonoffswitch").value = newLegends;
+		}
+
+		get legends() {
+			return this._shadowRoot.getElementById("myonoffswitch").value;
+		}
+		
 	}
 
 	customElements.define("com-sap-sample-piechart1-builder", RadialBarChartBuilderPanel);
