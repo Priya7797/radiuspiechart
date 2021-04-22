58522872a9866d88622fe6a9a9595bc143f29c4a
Priya7797 <81359264+Priya7797@users.noreply.github.com>
2021-04-22 20:44:56 +0530

    Update demupiechartbuilder.js

--- demupiechartbuilder.js
+++ demupiechartbuilder.js
@@ -34,7 +34,7 @@
 			this._shadowRoot = this.attachShadow({mode: "open"});
 			this._shadowRoot.appendChild(template.content.cloneNode(true));
 			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
-			this._shadowRoot.getElementById("showlegend").addEventListener("click", this._submit.bind(this));
+// 			this._shadowRoot.getElementById("showlegend").addEventListener("click", this._submit.bind(this));
 		}
 
 		_submit(e) {
