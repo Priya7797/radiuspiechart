33c13ef4324530d209bc435646d61c535375238d
Priya7797 <81359264+Priya7797@users.noreply.github.com>
2021-04-22 17:01:41 +0530

    Update demupiechartbuilder.js

--- demupiechartbuilder.js
+++ demupiechartbuilder.js
@@ -11,66 +11,24 @@
 					<td>Chart Title Font Size</td>
 					<td><input id="chart_title_fontsize" type="number" size="2" maxlength="2"></td>
 				</tr
-</table>
-<div class="onoffswitch">
-<span> Legend </span>
-    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" tabindex="0" checked>
-    <label class="onoffswitch-label" for="myonoffswitch">
-        <span class="onoffswitch-inner"></span>
-        <span class="onoffswitch-switch"></span>
-    </label>
-</div>
+				<br/>
+				<tr>
+					<span>Do you want legend?</span>
+					<label for="chkYes">
+					    <input type="radio" id="chkYes" name="chklegend" checked/>
+					    Yes
+					</label>
+					<label for="chkNo">
+					    <input type="radio" id="chkNo" name="chklegend" />
+					    No
+					</label>
+				</tr>
+			</table>
+
 			<input type="submit" style="display:none;">
 		</form>
 		<style>
-.onoffswitch {
-    position: relative; width: 90px;
-    -webkit-user-select:none; -moz-user-select:none; -ms-user-select: none;
-}
-.onoffswitch-checkbox {
-    position: absolute;
-    opacity: 0;
-    pointer-events: none;
-}
-.onoffswitch-label {
-    display: block; overflow: hidden; cursor: pointer;
-    border: 2px solid #999999; border-radius: 20px;
-}
-.onoffswitch-inner {
-    display: block; width: 200%; margin-left: -100%;
-    transition: margin 0.3s ease-in 0s;
-}
-.onoffswitch-inner:before, .onoffswitch-inner:after {
-    display: block; float: left; width: 50%; height: 30px; padding: 0; line-height: 30px;
-    font-size: 14px; color: white; font-family: Trebuchet, Arial, sans-serif; font-weight: bold;
-    box-sizing: border-box;
-}
-.onoffswitch-inner:before {
-    content: "ON";
-    padding-left: 10px;
-    background-color: #34A7C1; color: #FFFFFF;
-}
-.onoffswitch-inner:after {
-    content: "OFF";
-    padding-right: 10px;
-    background-color: #EEEEEE; color: #999999;
-    text-align: right;
-}
-.onoffswitch-switch {
-    display: block; width: 18px; margin: 6px;
-    background: #FFFFFF;
-    position: absolute; top: 0; bottom: 0;
-    right: 56px;
-    border: 2px solid #999999; border-radius: 20px;
-    transition: all 0.3s ease-in 0s; 
-}
-.onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-inner {
-    margin-left: 0;
-}
-.onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-switch {
-    right: 0px; 
-}
-		:host {
+			:host {
 			display: block;
 			padding: 1em 1em 1em 1em;
 		}
@@ -83,7 +41,7 @@
 			this._shadowRoot = this.attachShadow({mode: "open"});
 			this._shadowRoot.appendChild(template.content.cloneNode(true));
 			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
-			this._shadowRoot.getElementById("myonoffswitch").addEventListener("click", this._submit.bind(this));
+			this._shadowRoot.getElementByName("chklegend").addEventListener("click", this._submit.bind(this));
 		}
 
 		_submit(e) {
@@ -115,11 +73,11 @@
 			return this._shadowRoot.getElementById("chart_title_fontsize").value;
 		}
 		set legend(newLegend) {
-			this._shadowRoot.getElementById("myonoffswitch").checked = newLegend;
+			this._shadowRoot.getElementByName("chklegend").checked = newLegend;
 		}
 
 		get legend() {
-			return this._shadowRoot.getElementById("myonoffswitch").checked;
+			return this._shadowRoot.getElementByName("chklegend").checked;
 		}
 		
 	}
