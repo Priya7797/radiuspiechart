b97a7388d816941ef73342f1305ab2d46e48853f
Priya Singh <priya7797singh@gmail.com>
2021-05-03 00:13:28 +0530

    c

--- demopiechart.js
+++ demopiechart.js
@@ -232,24 +232,24 @@ if(this._legendValue == false){
 	series.alignLabels = false;
 	series.labels.template.radius = am4core.percent(-40);
 	series.labels.template.fill = am4core.color("white");
-	if(this._radioValue == true){
+}
+if(this._radioValue == true){
 	
-		series.labels.template.text = "{value}";
-	
-	}
-	else if(this._radioPercent == true)
-	{
-	
-		series.labels.template.text = "{value.percent.formatNumber('#.0')}%";
-	}
-	else if(this._radioCombination == true){
-		series.labels.template.text = "{value.percent.formatNumber('#.0')}% ({value})";
-		
-	}
-	else{
-		series.labels.template.text = "{value.percent.formatNumber('#.0')}%";
+	series.labels.template.text = "{value}";
 
-	}
+}
+else if(this._radioPercent == true)
+{
+
+	series.labels.template.text = "{value.percent.formatNumber('#.0')}%";
+}
+else if(this._radioCombination == true){
+	series.labels.template.text = "{value.percent.formatNumber('#.0')}% ({value})";
+	
+}
+else{
+	series.labels.template.text = "{value.percent.formatNumber('#.0')}%";
+
 }
 
 
