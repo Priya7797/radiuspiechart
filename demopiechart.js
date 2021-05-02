e886e97ae4c7694dce765d3860607cf5094f6a4d
Priya Singh <priya7797singh@gmail.com>
2021-05-03 00:42:18 +0530

    c

--- demopiechart.js
+++ demopiechart.js
@@ -236,6 +236,7 @@ if(this._legendValue == false){
 	series.labels.template.radius = am4core.percent(-70);
 	series.labels.template.fill = am4core.color("white");
 	series.labels.template.fontSize = 11;
+	series.labels.template.maxWidth = 60
 	inculde_category="";
 	
 }
